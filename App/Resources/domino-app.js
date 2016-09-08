967d732cd6495d44f83a2e27fd6dc8362a3a11b7
GeovannyMF <ing_gimf@hotmail.com>
2016-09-08 16:43:43 -0500

    Se agregran imagenes para avatar

--- App/Resources/domino-app.js
+++ App/Resources/domino-app.js
@@ -3,8 +3,17 @@ app.controller('AvatarController', function ($scope, $http)
 {
     //carga inicial de avatars
 	var avatars = new Array();
-	avatars[0] = {Name:"Celia Cruz", Photo:"celia"};
-	avatars[1] = {Name:"Obama", Photo:"obama"};
-	avatars[2] = {Name:"Hugo Chavez", Photo:"hugo"};
+	avatars[0] = {Name:"Andrés Manuel", Photo:"amlo"};
+	avatars[1] = {Name:"Celia Cruz", Photo:"celia"};
+	avatars[2] = {Name:"Hugo Chavez", Photo:"chavez"};
+	avatars[3] = {Name:"Che Guevara", Photo:"che"};
+	avatars[4] = {Name:"Compay", Photo:"compay"};
+	avatars[5] = {Name:"Fidel Castro", Photo:"fidel"};
+	avatars[6] = {Name:"Kim", Photo:"kim"};
+	avatars[7] = {Name:"Maduro", Photo:"maduro"};
+	avatars[8] = {Name:"Obama", Photo:"obama"};
+	avatars[9] = {Name:"Enrique Peña", Photo:"pena"};
+	avatars[10] = {Name:"Putin", Photo:"putin"};
+	avatars[11] = {Name:"Trump", Photo:"trump"};
 	$scope.modelAvatars = avatars;	
 });
