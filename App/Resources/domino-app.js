49312429efc3a34b81d278e9fef98c8657bdafad
GeovannyMF <ing_gimf@hotmail.com>
2016-09-08 15:49:33 -0500

    Cargar inicial de app
    carga inicial de app

---
+++ App/Resources/domino-app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('DominoApp', []);
+app.controller('AvatarController', function ($scope, $http) 
+{
+    //carga inicial de avatars
+	var avatars = new Array();
+	avatars[0] = {Name:"Celia Cruz", Photo:"celia"};
+	avatars[1] = {Name:"Obama", Photo:"obama"};
+	avatars[2] = {Name:"Hugo Chavez", Photo:"hugo"};
+	$scope.modelAvatars = avatars;	
+});
