67091fcbf7d7e879f1007858c0064387371bf3e4
4ndr05 <this.is.andros@gmail.com>
2016-09-08 22:37:02 -0500

    Call to Services

--- App/server.js
+++ App/server.js
@@ -1,23 +1,28 @@
 // Setup basic express server
+var util = require('util');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = 8081;
+var request = require('request');
+
+
+var serverPort = 8081;
+var APIServer = 'http://la-match-02:8080/domino/v1'
 
 // Routing
 app.use(express.static(__dirname + '/Resources'));
 
 app.get('/', function (req, res) {
-   res.sendFile(__dirname + "/" + "index.html" );
+    res.sendFile(__dirname + "/" + "index.html" );
 })
 
 app.get('/index.html', function (req, res) {
-   res.sendFile(__dirname + "/" + "index.html" );
+    res.sendFile(__dirname + "/" + "index.html" );
 })
 
 /*
-app.post('/process_post', urlencodedParser, function (req, res) 
+app.post('/process_post', urlencodedParser, function (req, res)
 {
 
    // Prepare output in JSON format
@@ -29,26 +34,48 @@ app.post('/process_post', urlencodedParser, function (req, res)
    res.end(JSON.stringify(response));
 })*/
 
-server.listen(port, function () {
-  console.log("Example app listening at http://%s:%s", server.address().address, server.address().port)
+server.listen(serverPort, function () {
+    console.log("Domino listening at http://%s:%s", server.address().address, server.address().port)
 })
 
 function addParticipant(data) {
 
-  //Agregar participante a juego o crear juego, se obtiene id del juego
-  var matchId = "xxxx";
-  //Agregar participante al room del juego
-  data.join(matchId);
-  //Avisar a los demas jugadores
-  io.to(matchId).emit('NuevoJugador', { Name: "castro", photo: "castro" });
-  //socket.emit('news', { hello: 'world' });
+    var currentGameId;
+    var joined = false;
+
+    //Obtener la lista de juegos creados
+    request.get(util.format('%s/matches/',APIServer), function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body);
+
+            //Tratamos de unir al usuario en alguno de los juegos
+            currentGameId = obj.matches.items[0].matchid
+            console.log(currentGameId);
+            request.post({url:util.format('%s/matches/%s/players/', APIServer, currentGameId)}, function(error, response, body) {
+                //En caso de que se una el jugador, lo metemos al canal del juego
+                if(!error && response.statusCode == 201) {
+                    console.log(util.format('%s %s %s', currentGameId, 'castro', 'castroimg'));
+                    data.join(currentGameId);
+                    io.to(currentGameId).emit('NuevoJugador', { Name: "castro", photo: "castro" });
+                }
+            });
+        }
+        else {
+            return false;
+        }
+    });
+
+    //socket.emit('news', { hello: 'world' });
 	//socket.broadcast.emit('new_user');
 }
 
 io.on('connection', function (socket) {
-	console.log("Usuario conectado - " + socket.id);
-  addParticipant(socket);
-	socket.on('disconnect', function () {
+
+    console.log("Usuario conectado - " + socket.id);
+    addParticipant(socket);
+
+    socket.on('disconnect', function () {
 	  //io.emit('player_diconected', { player: 'xxxxx' } );
 	});
 });
